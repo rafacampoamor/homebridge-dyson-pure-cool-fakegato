d0dffcf2ff86a129d565d7bc008eb05bd663e3f4
Rafa Campoamor <rafacampoamor@gmail.com>
2022-12-23 12:28:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const device = require('./src/dyson-pure-cool-device').device.info.name;
  */
 module.exports = function (homebridge) {
     homebridge.registerPlatform('homebridge-dyson-pure-cool', 'DysonPureCoolPlatform', DysonPureCoolPlatform, true);
-    var FakeGatoHistoryService = require('fakegato-history')(homebridge);
-    Accessory.log = this.log;
-    this.loggingService.addEntry({time: Math.round(new Date().valueOf() / 1000), temp: this.temperature, humidity: this.humidity, ppm: this.ppm});
+    const FakeGatoHistoryService = require('fakegato-history')(homebridge);
     const historyService = new FakeGatoHistoryService(device.info.name, UUIDGen, global.fakegatoOptions);
+
+    historyService.addEntry({time: Math.round(new Date().valueOf() / 1000), temp: this.temperature, humidity: this.humidity, ppm: this.ppm});
+    accessory.addService(historyService);
+
+
 
 }
